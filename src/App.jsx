897070ef8b8a6e82114c812100dc1db83fce2513
PabloRafael-coder <pabloofael@gmail.com>
2024-08-06 22:13:25 -0300

    feat: Add project's structure and importe styles global, app.module and svg image

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import styles from './App.module.css'
+
+import rocket from './assets/rocket.svg'
+import "./global.css"
 function App() {
 
   return (
     <>
-
+      <header className={styles.header}>
+        <img src={rocket} alt="imagem de um foguete" />
+        <h1 className={styles.headerText}>to<span>do</span></h1>
+      </header>
+      <main>
+        <div className={styles.taskInputContainer}>
+          <input type="text" placeholder="Adicione uma nova tarefa" />
+          <button>Criar</button>
+        </div>
+      </main>
     </>
   )
 }
