ff51a4b7f65561ff6ef755b93ab3c5449c242e62
PabloRafael-coder <pabloofael@gmail.com>
2024-08-08 22:26:54 -0300

    feat: Add image to app and create new section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import styles from './App.module.css'
 
 import rocket from './assets/rocket.svg'
+import plus from './assets/plus.svg';
+import clipboard from './assets/Clipboard.svg'
 import "./global.css"
 function App() {
 
@@ -8,12 +10,23 @@ function App() {
     <>
       <header className={styles.header}>
         <img src={rocket} alt="imagem de um foguete" />
-        <h1 className={styles.headerText}>to<span>do</span></h1>
+        <h1 className={styles.headerTitle}>to<span>do</span></h1>
       </header>
       <main>
-        <div className={styles.taskInputContainer}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
-          <button>Criar</button>
+        <section className={styles.taskInputContainer}>
+          <input className={styles.taskInput} type="text" placeholder="Adicione uma nova tarefa" />
+          <button className={styles.taskInputButton}>Criar <img src={plus} /></button>
+        </section>
+        <div className={styles.taskListContainer}>
+          <header>
+            <p>Tarefas criadas<span>0</span></p>
+            <p>Concluídas<span>0</span></p>
+          </header>
+          <div>
+            <img src={clipboard} />
+            <p>Você ainda não tem tarefas cadastradas</p>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
         </div>
       </main>
     </>
