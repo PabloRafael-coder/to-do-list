f11f8b83a2297ac17ee45f8d29c2116f7a421cda
PabloRafael-coder <pabloofael@gmail.com>
2024-08-13 22:35:49 -0300

    feat: Add div taskListContent in section and Implement styles on taskListContent

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,25 @@ function App() {
         <h1 className={styles.headerTitle}>to<span>do</span></h1>
       </header>
       <main>
-        <section className={styles.taskInputContainer}>
-          <input className={styles.taskInput} type="text" placeholder="Adicione uma nova tarefa" />
-          <button className={styles.taskInputButton}>Criar <img src={plus} /></button>
-        </section>
-        <div className={styles.taskListContainer}>
-          <header>
-            <p>Tarefas criadas<span>0</span></p>
-            <p>Concluídas<span>0</span></p>
-          </header>
-          <div>
-            <img src={clipboard} />
-            <p>Você ainda não tem tarefas cadastradas</p>
-            <p>Crie tarefas e organize seus itens a fazer</p>
+        <section>
+          <div className={styles.taskInputContainer}>
+            <input className={styles.taskInput} type="text" placeholder="Adicione uma nova tarefa" />
+            <button className={styles.taskInputButton}>Criar <img src={plus} /></button>
           </div>
-        </div>
+          <div className={styles.taskListContainer}>
+            <header className={styles.taskListHeader}>
+              <p className={styles.headerTextCreated}>Tarefas criadas<span>0</span></p>
+              <p className={styles.headerTextCompleted}>Concluídas<span>0</span></p>
+            </header>
+            <div className={styles.taskListContent}>
+              <img src={clipboard} />
+              <div className={styles.contentListText}>
+                <p className={styles.taskListText}>Você ainda não tem tarefas cadastradas</p>
+                <p>Crie tarefas e organize seus itens a fazer</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   )
