3fddfa1767fc3a34112d325591cc7ff858ba6e55
PabloRafael-coder <pabloofael@gmail.com>
2024-08-19 23:05:56 -0300

    feat: Create component TaskList

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,22 @@
+import styles from './TaskList.module.css'
+
+import trash from '../assets/trash.svg'
+
+export function TaskList(props) {
+    console.log(props)
+    return (
+        <>
+            <div className={styles.taskList}>
+                <div>
+                    <input type="checkbox" />
+                </div>
+                <p>
+                    {props.content}
+                </p>
+                <div>
+                    <img src={trash} alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
