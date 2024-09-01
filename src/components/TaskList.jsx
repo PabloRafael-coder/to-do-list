d2f963f588f5f80a903668e9a9d6a8ad010ca067
PabloRafael-coder <pabloofael@gmail.com>
2024-09-01 17:19:04 -0300

    feat: Adicionar função para manter o conteúdo para ser deletado. Implementar o button para deletar a tarefa.

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,19 +1,24 @@
 import styles from './TaskList.module.css'
 import { Trash } from '@phosphor-icons/react'
 
-export function TaskList({ content }) {
+export function TaskList({ content, onDeleteTask }) {
+
+    function handleDeleteTask() {
+        onDeleteTask(content);
+    }
+
     return (
         <>
             <div className={styles.taskElementContainer}>
                 <div className={styles.inputContainer}>
-                    <input type="checkbox" />
+                    <input type="checkbox"></input>
                 </div>
                 <div className={styles.textContainer}>
                     <p>{content}</p>
                 </div>
-                <div className={styles.trashContainer}>
-                    <Trash className={styles.colorTrash} />
-                </div>
+                <button onClick={handleDeleteTask} className={styles.trashContainer}>
+                    <Trash size={16} className={styles.colorTrash} />
+                </button>
             </div>
         </>
     )
