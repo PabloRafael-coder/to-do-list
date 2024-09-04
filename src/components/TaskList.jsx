80c1d616448e6d7be7040f890081a54d261b2c85
PabloRafael-coder <pabloofael@gmail.com>
2024-09-03 22:54:44 -0300

    feat: Adicionar função para manipular verificar o input se é true ou false.  Adicionar condicional no css para aplicar estilo no texto conforme o checked altera o valor.

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react'
 import styles from './TaskList.module.css'
 import { Trash } from '@phosphor-icons/react'
 
-export function TaskList({ content, onDeleteTask }) {
+export function TaskList({ content, onDeleteTask, isChecked, onIncreaseNumberCompletedTasks }) {
+    const [checked, setChecked] = useState(isChecked)
+
+    function handleInputTask(event) {
+        const taskChecked = event.target.checked
+        setChecked(taskChecked)
+        onIncreaseNumberCompletedTasks(taskChecked)
+    }
 
     function handleDeleteTask() {
         onDeleteTask(content);
@@ -11,10 +19,10 @@ export function TaskList({ content, onDeleteTask }) {
         <>
             <div className={styles.taskElementContainer}>
                 <div className={styles.inputContainer}>
-                    <input type="checkbox"></input>
+                    <input type="checkbox" checked={checked} onChange={handleInputTask}></input>
                 </div>
                 <div className={styles.textContainer}>
-                    <p>{content}</p>
+                    <p className={checked ? styles.textDecorate : styles.textWithoutDecorate}>{content}</p>
                 </div>
                 <button onClick={handleDeleteTask} className={styles.trashContainer}>
                     <Trash size={16} className={styles.colorTrash} />
